bc067894e2b6e85188fc48f17ccfe9f3a6eec7d8
Sophia T <114521723+sztoscano@users.noreply.github.com>
2022-11-14 23:06:28 -0600

    Add files via upload

---
+++ Component0.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+export default () => {
+  return (
+    <View style = {
+      {
+        "flexDirection": "row",
+        "alignItems": "flex-start"
+      }
+    } >
+    <View style = {
+      {
+        "alignItems": "flex-start"
+      }
+    } >
+    <View style = {
+      {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+      }
+    } > <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginTop": 48.22
+      }
+    } >
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 132.85,
+        "height": 76.87,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    </View>
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": -42.1
+      }
+    } >
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 183.8,
+        "height": 77.5,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    </View>
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 183.23,
+        "height": 76.57,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 14.87,
+        "height": 19.81,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    </View>
+    <View style = {
+      {
+        flexDirection: 'row',
+        alignItems: 'flex-start'
+      }
+    } > {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 119.1,
+        "height": 79.22,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 122.72,
+        "height": 73.36,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 16.07,
+        "height": 17.85,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": -119.09,
+        "marginTop": -21.96
+      }
+    } >
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 124.46,
+        "height": 86.31,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    </View>
+    </View>
+    </View>
+    <View style = {
+      {
+        "alignItems": "flex-start",
+        "marginStart": -125.26
+      }
+    } >
+
+    {
+      /* <Path /> {Path is not supported. It can be exported as Svg} */ }
+    <View style = {
+      {
+        "width": 125.23,
+        "height": 143.39,
+        "backgroundColor": "#000000"
+      }
+    }
+    />
+    </View>
+    </View>
+
+  );
+};
